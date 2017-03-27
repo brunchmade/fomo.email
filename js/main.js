1b7966dfb97904eb8a2a9a0be20fff6bddf8cbba
Wells Riley <wells.riley@gmail.com>
2017-03-26 23:06:21 -0700

    minor tweaks

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
   $('.js-previewOpen').on("click", function(){
-    $('.emailcontainer').fadeIn();
+    $('.emailcontainer').fadeIn(200);
     $('.emailwindow').css("bottom", "0");
   });
 
   $('.js-previewClose').on("click", function(){
-    $('.emailcontainer').fadeOut();
+    $('.emailcontainer').fadeOut(200);
     $('.emailwindow').css("bottom", "-95vh");
   });
 });
